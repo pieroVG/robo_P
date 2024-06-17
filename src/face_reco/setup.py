7d427e02ad7b5afc248ce61a01ec8d9426baf103
pieroVG <piero.vega-gutierrez@univ-tlse3.fr>
2024-06-17 21:16:42 +0200

    creation fichiers

---
+++ src/face_reco/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'face_reco'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='piero',
+    maintainer_email='piero.vega-gutierrez@univ-tlse3.fr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
