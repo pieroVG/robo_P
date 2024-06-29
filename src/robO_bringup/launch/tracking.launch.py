aa66863fae5150143283152a22628c7482e130d6
pieroVG <piero.vega-gutierrez@univ-tlse3.fr>
2024-06-29 20:25:38 +0200

    tracking.launch avec camera

--- src/robO_bringup/launch/tracking.launch.py
+++ src/robO_bringup/launch/tracking.launch.py
@@ -4,6 +4,19 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
+    camera = Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            name='camera',
+            parameters=[{
+                'video_device': '/dev/video0',
+                'image_size': [640, 480],
+                'pixel_format': 'YUYV',
+                'output_encoding': 'rgb8'
+            }],
+            output='screen'
+    )
+
     detect_face = Node(
         package='face_reco',
         executable='detect_face',
@@ -14,6 +27,7 @@ def generate_launch_description():
         executable='tracker_face',
     )
 
+    ld.add_action(camera)
     ld.add_action(detect_face)
     ld.add_action(tracker_face)
 
